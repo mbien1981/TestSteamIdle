64dc1743c4b350f7daed6f8dd6f34c1cc3f13428
Uziel <clauselpitoloko@gmail.com>
2021-07-27 18:22:59 -0300

    Added TestSteamIdleGUI

---
+++ TestSteamIdleGUI/TestSteamIdleGUI/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+
+namespace TestSteamIdleGUI
+{
+    public partial class mainWindow : Form {
+        const string AppIdsTxt = "apps.txt";
+        const string IdleExeName = "TestSteamIdle";
+        const string IdleExe = IdleExeName + ".exe";
+
+        public mainWindow() {
+            InitializeComponent();
+        }
+
+        private void stopExistingIdle() {
+            if (Process.GetProcessesByName(IdleExeName).Length != 0) {
+                StartIdleButton.Enabled = false;
+
+                foreach (var process in Process.GetProcessesByName(IdleExeName))
+                    process.Kill();
+
+                printText("\nStopped existing idle process.");
+
+                StartIdleButton.Enabled = true;
+            }
+        }
+
+        private void printText(String input) {
+            OutputCase.AppendText(input + "\n");
+        }
+
+        private void startIdle_Button(object sender, EventArgs e) {
+            stopExistingIdle();
+
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            string pathTxt = Path.Combine(path, AppIdsTxt);
+            string pathExe = Path.Combine(path, IdleExe);
+            ProcessStartInfo startInfo = new ProcessStartInfo(pathExe);
+
+            printText("\nStarting Idle...");
+
+            foreach (string AppId in File.ReadAllText(pathTxt).Split(',')) {
+                startInfo.Arguments = AppId;
+                Process.Start(startInfo);
+
+                printText("\t• Started idle process for app " + AppId);
+
+                int index = OutputCase.Text.IndexOf(AppId);
+                int lenght = AppId.Length;
+
+                OutputCase.Select(index, lenght);
+                OutputCase.SelectionColor = Color.FromArgb(255, 100, 200, 100);
+            }
+
+            printText("\nIdling a total of " + Process.GetProcessesByName(IdleExeName).Length + " game(s).");
+        }
+
+        private void killIdleButton_Click(object sender, EventArgs e) {
+            stopExistingIdle();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e) {
+            printText("Initialized...\n");
+            stopExistingIdle();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OutputCase.Clear();
+            printText("Cleared.");
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -1,9 +1,16 @@
-# About
+## About
 A simple C# program to idle hours on steam.
 
-# Instructions
-Add the ids of the steam apps you want to idle inside "Apps.txt" located in the same directory where both dll and exe files are placed, the ids must be in csv format.
+## Instructions
+* Add the ids of the steam apps you want to idle inside an "**apps.txt**"  file located in the same directory where both dll and exe files are placed, the ids **must be** in csv format.
 
-Run "TestSteamIdleConsole.exe"
+You can either run "**TestSteamIdleConsole.exe**" or "**TestSteamIdleGUI.exe**" in order to use this program.
 
-Your steam client must be running while using this program.
+**Your steam client must be running while using this program.**
+
+
+# Preview
+<div style="text-align:center">
+	<img src="https://i.imgur.com/lwZ9TdR.png" />
+	<img src="https://i.imgur.com/CpZRYl5.png" />
+</div>
